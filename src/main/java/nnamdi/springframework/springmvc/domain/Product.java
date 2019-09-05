d224705cf6f03c1829fb006177546e2fd844588d
nnamdi16 <nwabuokeinnamdi@gmail.com>
2019-09-05 13:48:25 +0100

    Create a class and methods for setting and getting the properties(fields) of the product

--- src/main/java/nnamdi/springframework/springmvc/domain/Product.java
+++ src/main/java/nnamdi/springframework/springmvc/domain/Product.java
@@ -1,4 +1,39 @@
 package nnamdi.springframework.springmvc.domain;
-
+import java.math.BigDecimal;
 public class Product {
+    private  Integer id;
+    private String description;
+    private BigDecimal price;
+    private String imageUrl;
+
+    public Integer getId() {
+        return id;
+    }
+    public void setId (Integer id) {
+        this.id = id;
+    }
+    public String getDescription() {
+       return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
 }

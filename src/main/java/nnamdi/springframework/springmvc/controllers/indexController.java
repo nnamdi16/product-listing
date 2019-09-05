4d445fb11fd24ee94dc3f2b210a8897f56213d5a
nnamdi16 <nwabuokeinnamdi@gmail.com>
2019-09-05 10:45:36 +0100

    Mapping indexController to the root

--- src/main/java/nnamdi/springframework/springmvc/controllers/indexController.java
+++ src/main/java/nnamdi/springframework/springmvc/controllers/indexController.java
@@ -1,4 +1,16 @@
 package nnamdi.springframework.springmvc.controllers;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by Nwabuokei Nnamdi
+ */
+
+@Controller
 public class indexController {
+    @RequestMapping("/")
+    public String index() {
+        return "index";
+    }
 }
